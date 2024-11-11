a8c788c59f2563dfe42198b928fd3c271e6565af
TatianaManika <tati.manika@gmail.com>
2024-11-11 18:27:11 +0200

    Update AM_Generator.java

--- src/AM_Generator.java
+++ src/AM_Generator.java
@@ -4,5 +4,6 @@ public class AM_Generator {
         System.out.println("AM 2 : 8220151");
         System.out.println("AM 3 : 8220104");
         System.out.println("AM 4 : 8230069");
+        System.out.println("AM 5 : 8150188");
     }
 }
